b1e8e2742c7802de1888a7c72bc36134e4802860
Adam Winiarczuk <adamosca@gmail.com>
2018-07-22 18:36:55 +0200

    MISC run octo as build step

---
+++ octo/src/lib.rs
@@ -0,0 +1,7 @@
+extern crate lalrpop_util;
+extern crate octo_parser;
+
+pub fn hello() {
+    println!("Hello world!");
+}
+pub mod semantics;
---
+++ octo_showcase/build.rs
@@ -0,0 +1,5 @@
+extern crate octo;
+
+fn main() {
+    octo::hello();
+}
