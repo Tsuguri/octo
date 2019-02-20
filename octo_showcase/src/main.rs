142013b20c293323d56e5cf38938c4cdd05d4f46
Adam Winiarczuk <awiniarczuk@veritystudios.com>
2019-02-20 14:33:30 +0100

    RUST updating edition intro

--- octo_showcase/src/main.rs
+++ octo_showcase/src/main.rs
@@ -1,10 +1,19 @@
-extern crate env_logger;
-extern crate octo_runtime;
+//extern crate simple_logger;
+//extern crate octo_runtime;
+//extern crate log;
 use octo_runtime::main_function;
-use octo_runtime::winit;
+//use octo_runtime::winit;
+#[allow(unused_imports)]
+use log::{error, warn, info, debug, trace};
 
 fn main() {
-    env_logger::init();
+    //env_logger::init();
+    
     //let mut events_loop = winit::EventsLoop::new();
+    
+    simple_logger::init().unwrap();
+
+    info!("whoop whoop");
+
     main_function();//&mut events_loop);
 }
