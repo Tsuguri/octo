bdcb0118b2499e34c546a1ab6a5ddbf759fff879
Adam Winiarczuk <awiniarczuk@veritystudios.com>
2019-02-28 22:57:37 +0100

    GFX clearing window with mouse pos based color
    based on Lokathor tutorial. octo_showcase project.

--- octo_runtime/src/lib.rs
+++ octo_runtime/src/lib.rs
@@ -1,27 +1,3 @@
-#![cfg_attr(
-    not(any(feature = "vulkan", feature = "dx12", feature = "gl")),
-    allow(dead_code, unused_extern_crates, unused_imports)
-)]
-
-#[cfg(feature = "dx12")]
-pub extern crate gfx_backend_dx12 as backend;
-#[cfg(feature = "gl")]
-pub extern crate gfx_backend_gl as backend;
-#[cfg(feature = "vulkan")]
-pub extern crate gfx_backend_vulkan as backend;
-pub extern crate gfx_hal as hal;
-
-#[cfg(feature = "gl")]
-use back::glutin::GlContext;
-
-extern crate glsl_to_spirv;
-pub extern crate image;
-pub extern crate winit;
-
-mod data_loading;
-mod functionality;
-mod generated_stub;
-mod types;
-mod window;
-pub use crate::functionality::*;
-//pub use generated_stub::Pipeline;
+pub fn lolz() {
+    println!("lolz");
+}
--- octo_runtime/src/types.rs
+++
@@ -1,5 +0,0 @@
-#[derive(Debug, Clone, Copy)]
-pub struct Vertex {
-    pub pos: [f32; 2],
-    pub uv: [f32; 2],
-}
